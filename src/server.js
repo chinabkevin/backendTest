3c6c6a143748dbf31e2b94d8db6ff9755d1ffa73
kevinjam <kevin.janvier5@gmail.com>
2025-07-18 13:38:15 +0300

    add cron

--- src/server.js
+++ src/server.js
@@ -6,10 +6,15 @@ import rateLimiter from './middleware/rateLimiter.js';
 import freelancerRoute from './routes/freelancerRoute.js';
 import caseRoute from './routes/caseRoute.js';
 import userRoute from './routes/userRoute.js';
+import job from './config/cron.js';
 
 dotenv.config();
 
 const app = express();
+
+if(process.env.NODE_ENV === 'production'){
+  job.start();
+}
 
 // Allow CORS for frontend
 app.use(cors({
